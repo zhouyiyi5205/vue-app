b4063f333ebddc5156284ac54622f27bad212edf
zhouyiyi5205 <936810496@qq.com>
2020-08-19 10:48:47 +0800

    配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import HelloWorld from '@/pages/HelloWorld'
+import Home from '@/pages/Home'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/HelloWorld',
       name: 'HelloWorld',
       component: HelloWorld
-    }
+      // component: (resolve) => require(['@pages/HelloWorld'], resolve)
+    },
+    {
+      path: '/Home',
+      name: 'Home',
+      component: Home
+    },
   ]
 })

c259c6c481c05645f81cea3bf172e5f2427b5f09
zhouyiyi5205 <936810496@qq.com>
2020-09-14 17:34:05 +0800

    示例代码

---
+++ src/views/Example/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="home-page">
+<!-- 
+    <van-cell is-link @click="showPopup">展示弹出层</van-cell>
+    <van-popup v-model="show">内容</van-popup> -->
+
+    <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+      <van-swipe-item>1</van-swipe-item>
+      <van-swipe-item>2</van-swipe-item>
+      <van-swipe-item>3</van-swipe-item>
+      <van-swipe-item>4</van-swipe-item>
+    </van-swipe>
+
+    <van-grid>
+      <van-grid-item icon="photo-o" text="文字" />
+      <van-grid-item icon="photo-o" text="文字" />
+      <van-grid-item icon="photo-o" text="文字" />
+      <van-grid-item icon="photo-o" text="文字" />
+    </van-grid>
+
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+
+  </div>
+</template>
+
+<script>
+import { Button, Popup } from 'vant';
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+      show: false,
+    }
+  },
+  methods: {
+    showPopup() {
+      this.show = true;
+    },
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less">
+.home-page {
+  line-height: 40px;
+  height: 40px;
+  font-size: 16px;
+  .my-swipe .van-swipe-item {
+    color: #fff;
+    font-size: 20px;
+    line-height: 150px;
+    text-align: center;
+    background-color: #39a9ed;
+  }
+
+}
+</style>

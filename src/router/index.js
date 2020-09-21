9702f474a3ee11759da1e6ffd5ba79b4b61cc403
zhouyiyi5205 <936810496@qq.com>
2020-09-21 09:13:48 +0800

    修改路由

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,10 @@ import HelloWorld from '@/views/HelloWorld'
 import Home from '@/views/Home'
 import Example from '@/views/Example'
 import IDCardExample from '@/views/Example/IDCardExample'
+import HomeNoRealName from '@/views/Home/HomeNoRealName'
+import HomeNoBound from '@/views/Home/HomeNoBound'
+import HomeAll from '@/views/Home/HomeAll'
+
 Vue.use(Router)
 
 export default new Router({
@@ -13,6 +17,25 @@ export default new Router({
       name: 'Home',
       component: Home
     },
+    // ---- 未实名已绑定
+    {
+      path: '/Home/HomeNoRealName',
+      name: 'HomeNoRealName',
+      component: HomeNoRealName
+    },
+    //---- 已实名未绑定
+    {
+      path: '/Home/HomeNoBound',
+      name: 'HomeNoBound',
+      component: HomeNoBound
+    },
+    // ----- 已实名已绑定
+    {
+      path: '/Home/HomeAll',
+      name: 'HomeAll',
+      component: HomeAll
+    },
+    // ----- 示例路由 后期可删除
     {
       path: '/HelloWorld',
       name: 'HelloWorld',
